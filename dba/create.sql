1472ea78aa4dd1f29b972a7e6f57a5b7531bf55d
Allyson-English <allyson.english@gmail.com>
2021-08-08 03:07:03 +0000

    query format

--- dba/create.sql
+++ dba/create.sql
@@ -10,3 +10,16 @@ CREATE TABLE IF NOT EXISTS roadtrip.images (
     date_taken timestamp,
     geom GEOMETRY(Point, 4326)
 );
+
+
+sql = f"""UPDATE roadtrip.images
+SET date_taken = 
+    TO_TIMESTAMP('2021-07-04 00:00:00', 'YYYY:MM:DD HH24:MI:SS')::timestamp, 
+    geom = ST_SetSRID(ST_Point(-110.31791672482119, 44.62389804561182), 4326)
+WHERE attachment_id = 'ANGjdJ8tXG1Yh45Sd8rRxYmZQ';"""
+
+with closing(psycopg2.connect(DB_CONNECTION)) as conn:
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute(sql)
+    conn.commit()
